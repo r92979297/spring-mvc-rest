c3e6d23ef21ac6af872b582daad895737a1fccbb
r929297 <R92979297@gmail.com>
2019-10-06 08:08:20 +0200

    missed anotation

--- src/main/java/com/fafik/domain/Category.java
+++ src/main/java/com/fafik/domain/Category.java
@@ -3,10 +3,12 @@ package com.fafik.domain;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 @Data
+@Entity
 public class Category {
 
     @Id
