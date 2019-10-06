0e505f03cb038b5a392049d91d201dd772876a13
r929297 <R92979297@gmail.com>
2019-10-06 08:10:59 +0200

    wring ID  anotation

--- src/main/java/com/fafik/domain/Category.java
+++ src/main/java/com/fafik/domain/Category.java
@@ -1,11 +1,11 @@
 package com.fafik.domain;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
